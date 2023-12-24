cd97e37dd9bf4da10ff4897b5fe901ff281819a4
Lucas Rattighieri <102265663+Lucas-Rattighieri@users.noreply.github.com>
2023-12-24 02:10:58 -0300

    Update ag.py

--- ag.py
+++ ag.py
@@ -31,7 +31,7 @@ class AlgoritmoGenetico:
         self.funcao = funcao
         self.num_variaveis = num_variaveis
         self.args = args
-        self.num_populacao = num_populacao
+        self.num_populacao = num_populacao // 2
         self.num_genes = num_genes
         self.num_geracoes = num_geracoes
         self.taxa_crossover = taxa_crossover
